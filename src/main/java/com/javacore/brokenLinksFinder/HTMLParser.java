5258210d14e31f2c0d928c404b7dee9792e82a1f
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-12-07 17:01:25 +0300

    Парсер HTML

---
+++ src/main/java/com/javacore/brokenLinksFinder/HTMLParser.java
@@ -0,0 +1,42 @@
+package com.javacore.brokenLinksFinder;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final class HTMLParser {
+    static List<String> getValues(final String html, Attribute attribute) {
+        List<String> result = new ArrayList<>();
+
+        if (html == null) {
+            return result;
+        }
+
+        Document doc = Jsoup.parse(html);
+
+        for (Element node : doc.getElementsByAttribute(attribute.toString())) {
+            result.add(node.attr(attribute.toString()));
+        }
+
+        return result;
+    }
+
+    public enum Attribute {
+        HREF("href"),
+        SRC("src");
+
+        private final String string;
+
+        Attribute(final String string) {
+            this.string = string;
+        }
+
+        @Override
+        public String toString() {
+            return string;
+        }
+    }
+}
